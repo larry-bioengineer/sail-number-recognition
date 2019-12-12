8e6f14d61739f6a42b957b80a215e2550f097c56
Lok Yiu To <lokyiuto.larry@gmail.com>
2019-12-13 01:31:38 +0800

    added sample OCR code

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,18 @@
-print("helloworld")
+# Larry To
+# Created on: 12/13/2019
+# Purpose: Read in an image and output the recognized characters 
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+def ocr_core(filename):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    return text
+
+print(ocr_core('Images/Sail Number During Race/PER-50.jpg'))
